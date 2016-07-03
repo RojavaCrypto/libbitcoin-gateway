1aa4d1553e289a8e32baa904ea10a61857be17ed
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-07-03 15:02:40 -0400

    added missing query functions

--- client/ws_client.py
+++ client/ws_client.py
@@ -1,25 +1,163 @@
-#!/usr/bin/env python
-
 import asyncio
+import json
+import sys
 import websockets
 
-import json
+async def test_fetch_last_height(websocket):
+    print("Testing fetch last_height...")
+
+    message = json.dumps({
+        "command": "fetch_last_height",
+        "id": 1,
+        "params": [
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_transaction(websocket):
+    print("Testing fetch transaction...")
+
+    message = json.dumps({
+        "command": "fetch_transaction",
+        "id": 1,
+        "params": [
+            "ee475443f1fbfff84ffba43ba092a70d291df233bd1428f3d09f7bd1a6054a1f"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_history(websocket):
+    print("Testing fetch history...")
+
+    message = json.dumps({
+        "command": "fetch_history",
+        "id": 1,
+        "params": [
+            "13ejSKUxLT9yByyr1bsLNseLbx9H9tNj2d"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_block_header(websocket):
+    print("Testing fetch block_header...")
+
+    message = json.dumps({
+        "command": "fetch_block_header",
+        "id": 1,
+        "params": [
+            "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_block_transaction_hashes(websocket):
+    print("Testing fetch block_transaction_hashes...")
+
+    message = json.dumps({
+        "command": "fetch_block_transaction_hashes",
+        "id": 1,
+        "params": [
+            "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_spend(websocket):
+    print("Testing fetch spend...")
+
+    message = json.dumps({
+        "command": "fetch_spend",
+        "id": 1,
+        "params": [
+            ("0530375a5bf4ea9a82494fcb5ef4a61076c2af807982076fa810851f4bc31c09",
+             0)
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_transaction_index(websocket):
+    print("Testing fetch transaction_index...")
+
+    message = json.dumps({
+        "command": "fetch_transaction_index",
+        "id": 1,
+        "params": [
+            "ee475443f1fbfff84ffba43ba092a70d291df233bd1428f3d09f7bd1a6054a1f"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_block_height(websocket):
+    print("Testing fetch block_height...")
+
+    message = json.dumps({
+        "command": "fetch_block_height",
+        "id": 1,
+        "params": [
+            "000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e"
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
+
+async def test_fetch_stealth(websocket):
+    print("Testing fetch stealth...")
+
+    message = json.dumps({
+        "command": "fetch_stealth",
+        "id": 1,
+        "params": [
+            "11", 419135
+        ]
+    })
+    print("Sending:", message)
+    await websocket.send(message)
+
+    response = json.loads(await websocket.recv())
+    print(json.dumps(response, indent=2))
 
 async def hello():
     async with websockets.connect('ws://localhost:8888') as websocket:
-
-        message = json.dumps({
-            "command": "fetch_last_height",
-            "id": 1,
-            "params": [
-            ]
-        })
-        print("Sending:", message)
-
-        await websocket.send(message)
-
-        response = await websocket.recv()
-        print(">", response)
+        await test_fetch_last_height(websocket)
+        await test_fetch_transaction(websocket)
+        await test_fetch_history(websocket)
+        await test_fetch_block_header(websocket)
+        await test_fetch_block_transaction_hashes(websocket)
+        await test_fetch_spend(websocket)
+        await test_fetch_transaction_index(websocket)
+        await test_fetch_block_height(websocket)
+        await test_fetch_stealth(websocket)
 
 asyncio.get_event_loop().run_until_complete(hello())
 
--- gateway/bs_module.py
+++ gateway/bs_module.py
@@ -1,4 +1,6 @@
+import binascii
 import logging
+import libbitcoin
 
 class BitcoinServerCallback:
 
@@ -13,8 +15,9 @@ class BitcoinServerCallback:
         return response(None, [])
 
     async def query(self):
-        if not self.check_request():
-            logging.error("Bad parameters specified: %s", exc, exc_info=True)
+        if not self.initialize(self._params):
+            logging.error("Bad parameters specified: %s",
+                          self._params, exc_info=True)
             return None
         return await self.make_query()
 
@@ -34,19 +37,194 @@ class BitcoinServerCallback:
             "result": result
         }
 
+def decode_hash(encoded_hash):
+    try:
+        decoded_hash = bytes.fromhex(encoded_hash)
+    except ValueError:
+        return None
+    if len(decoded_hash) != 32:
+        return None
+    return decoded_hash
+
+def encode_hex(value):
+    return binascii.hexlify(value).decode("ascii")
+
+def unpack_index(index):
+    if type(index) == str:
+        return decode_hash(index)
+    elif type(index) == int:
+        return index
+    else:
+        return None
+
 class BsFetchLastHeight(BitcoinServerCallback):
 
-    def check_request(self):
-        return len(self._params) == 0
+    def initialize(self, params):
+        return len(params) == 0
 
     async def make_query(self):
         ec, height = await self._client.last_height()
         return self.response(ec, [height])
 
+class BsFetchTransaction(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        self._tx_hash = decode_hash(params[0])
+        if self._tx_hash is None:
+            return False
+        return True
+
+    async def make_query(self):
+        ec, tx_data = await self._client.transaction(self._tx_hash)
+        tx_data = encode_hex(tx_data)
+        return self.response(ec, [tx_data])
+
+class BsFetchHistory(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1 and len(params) != 2:
+            return False
+        self._address = params[0]
+        self._from_height = 0
+        if len(params) == 2:
+            self._from_height = params[1]
+        return type(self._address) == str and type(self._from_height) == int
+
+    async def make_query(self):
+        ec, history = await self._client.history(self._address,
+                                                 from_height=self._from_height)
+        result = []
+        for point, height, value in history:
+            if type(point) == libbitcoin.OutPoint:
+                result.append({
+                    "type": "output",
+                    "point": point.tuple(),
+                    "height": height,
+                    "value": value,
+                    "checksum": point.checksum()
+                })
+            elif type(point) == libbitcoin.InPoint:
+                result.append({
+                    "type": "spend",
+                    "point": point.tuple(),
+                    "height": height,
+                    "outpoint_checksum": value
+                })
+        return self.response(ec, result)
+
+class BsFetchBlockHeader(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        self._index = unpack_index(params[0])
+        return True
+
+    async def make_query(self):
+        ec, header = await self._client.block_header(self._index)
+        header = encode_hex(header)
+        return self.response(ec, [header])
+
+class BsFetchBlockTransactionHashes(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        self._index = unpack_index(params[0])
+        return True
+
+    async def make_query(self):
+        ec, hashes = await self._client.block_transaction_hashes(self._index)
+        results = []
+        for hash in hashes:
+            results.append(encode_hex(hash))
+        return self.response(ec, results)
+
+class BsFetchSpend(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        point = params[0]
+        if len(point) != 2:
+            return False
+        self._outpoint = libbitcoin.OutPoint()
+        self._outpoint.hash = decode_hash(point[0])
+        if self._outpoint.hash is None:
+            return False
+        self._outpoint.index = point[1]
+        return type(self._outpoint.index) == int
+
+    async def make_query(self):
+        ec, spend = await self._client.spend(self._outpoint)
+        return self.response(ec, [spend.tuple()])
+
+class BsFetchTransactionIndex(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        self._tx_hash = decode_hash(params[0])
+        if self._tx_hash is None:
+            return False
+        return True
+
+    async def make_query(self):
+        ec, height, index = await self._client.transaction_index(self._tx_hash)
+        return self.response(ec, [height, index])
+
+class BsFetchBlockHeight(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1:
+            return False
+        self._block_hash = decode_hash(params[0])
+        if self._block_hash is None:
+            return False
+        return True
+
+    async def make_query(self):
+        ec, height = await self._client.block_height(self._block_hash)
+        return self.response(ec, [height])
+
+class BsFetchStealth(BitcoinServerCallback):
+
+    def initialize(self, params):
+        if len(params) != 1 and len(params) != 2:
+            return False
+        self._prefix = libbitcoin.Binary.from_string(params[0])
+        if self._prefix is None:
+            return False
+        self._from_height = 0
+        if len(params) == 2:
+            self._from_height = params[1]
+        return type(self._from_height) == int
+
+    async def make_query(self):
+        ec, rows = await self._client.stealth(self._prefix, self._from_height)
+        results = []
+        for ephemkey, address_hash, tx_hash in rows:
+            results.append({
+                "ephemeral_key": encode_hex(ephemkey),
+                "address_hash": encode_hex(address_hash),
+                "tx_hash": encode_hex(tx_hash)
+            })
+        return self.response(ec, results)
+
 class BitcoinServerModule:
 
     _handlers = {
-        "fetch_last_height":                BsFetchLastHeight
+        "fetch_last_height":                BsFetchLastHeight,
+        "fetch_transaction":                BsFetchTransaction,
+        "fetch_history":                    BsFetchHistory,
+        "fetch_block_header":               BsFetchBlockHeader,
+        "fetch_block_transaction_hashes":   BsFetchBlockTransactionHashes,
+        "fetch_spend":                      BsFetchSpend,
+        "fetch_transaction_index":          BsFetchTransactionIndex,
+        "fetch_block_height":               BsFetchBlockHeight,
+        "fetch_stealth":                    BsFetchStealth,
     }
 
     def __init__(self, client):
